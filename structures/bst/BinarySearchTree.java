25a78834bff18b61bf114b1088a6b20ee55892bb
ToxicCicity <121562161+ToxicCicity@users.noreply.github.com>
2024-04-30 13:52:23 +0300

    bst denemesi yapildi

---
+++ structures/bst/BinarySearchTree.java
@@ -0,0 +1,85 @@
+package structures.bst;
+
+public class BinarySearchTree {
+    Node root;
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
+    void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node root, int key) {
+        if (root == null)  return root;
+
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = minValue(root.right);
+
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    boolean searchKey(int key) {
+        return searchKeyRec(root, key);
+    }
+
+    boolean searchKeyRec(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (key == root.key) {
+            return true;
+        }
+        return key < root.key ? searchKeyRec(root.left, key) : searchKeyRec(root.right, key);
+        }
+
+
+}
---
+++ structures/bst/bst_deneme.java
@@ -0,0 +1,20 @@
+package structures.bst;
+
+public class bst_deneme {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+        tree.inorder();
+
+        tree.deleteKey(20);
+
+        System.out.println("Inorder traversal of the modified tree after deletion of 20");
+        tree.inorder();
+    }
+}
