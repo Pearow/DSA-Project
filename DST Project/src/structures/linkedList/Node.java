6102d9272846491168e79f2064a5995237ce7695
Azizcan Tam <tamazizcan407@gmail.com>
2024-05-01 19:57:46 +0300

    linkedlistnode

--- DST Project/src/structures/linkedList/Node.java
+++ DST Project/src/structures/linkedList/Node.java
@@ -1,4 +1,18 @@
 package src.structures.linkedList;
 
+import src.Student;
 public class Node {
+
+    private double gpa;
+    private Student data;
+    private  Node next;
+
+    public Node(Student data){
+        this.data=data;
+        this.gpa=data.getGpa;
+        this.next =null;
+    }
+
+
+
 }
