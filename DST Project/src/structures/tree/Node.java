21d3f5f0674137e2528ac44b4b99c1e86562a792
Pearow <salimmert2003@hotmail.com>
2024-05-01 23:27:33 +0300

    Adding Basic Tree Components

--- DST Project/src/structures/tree/Node.java
+++ DST Project/src/structures/tree/Node.java
@@ -4,18 +4,25 @@ import src.Student;
 
 import java.io.Serializable;
 
-public class Node implements Serializable {
-    private double gpa;
+public class Node implements Serializable, Comparable<Node> {
     private Student data;
     private Node left;
     private Node right;
-//    private Node Root;
+    private int balance;
+    private Node parent;
+
+    public Node(Node parent, Student data) {
+        this.data = data;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
 
     public Node(Student data) {
-//        this.data = data;
-//        this.gpa = data.getGpa();
-//        this.left = null;
-//        this.right = null;
+        this.data = data;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
     }
 
     public Student getData() {
@@ -40,5 +47,30 @@ public class Node implements Serializable {
 
     public void setRight(Node right) {
         this.right = right;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void increaseBalance() {
+        balance++;
+    }
+
+    public void decreaseBalance() {
+        balance--;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return 0;
     }
 }
--- DST Project/src/structures/tree/Tree.java
+++ DST Project/src/structures/tree/Tree.java
@@ -7,15 +7,97 @@ import java.io.Serializable;
 public class Tree implements Serializable {
     private Node root;
 
-    public int add(Student data){
-        return 0;
+    public void add(Student data) {
+        Node newNode = new Node(data);
+        add(root, null, newNode);
     }
 
-    public boolean remove(Student data){
-        return true;
+    private Node add(Node pointer, Node parent, Node newNode){
+        if(pointer == null) {
+            newNode.setParent(parent);
+            return newNode;
+        }
+        if(pointer.compareTo(newNode) >= 0){
+            pointer.increaseBalance();
+            pointer.setRight(add(pointer.getRight(), pointer, newNode));
+        }
+        else if(pointer.compareTo(newNode) < 0){
+            pointer.decreaseBalance();
+            pointer.setLeft(add(pointer.getLeft(), pointer, newNode));
+        }
+        return pointer;
     }
 
-    public Student find(int index){
+    public boolean remove(int id) throws Exception{
+        return remove(findNode(id));
+    }
+
+    private boolean remove(Node node) throws Exception{
+        if (node == null)
+            return false;
+        if (node.getLeft() == null && node.getRight() == null) {
+            if (node != root) {
+                if (node.getParent().getLeft() == node)
+                    node.getParent().setLeft(null);
+                else
+                    node.getParent().setRight(null);
+            } else
+                root = null;
+            return true;
+        } else if (node.getLeft() != null && node.getRight() != null) {
+                Node min = node;
+                while (min.getLeft() != null)
+                    min = min.getLeft();
+                node.setData(min.getData());
+                if (!remove(min))
+                    throw new Exception("Child node not found");
+                return true;
+
+        } else {
+            Node child = node.getLeft() != null ? node.getLeft() : node.getRight();
+            if (node != root) {
+                if (node.getParent().getLeft() == node)
+                    node.getParent().setLeft(child);
+                else
+                    node.getParent().setRight(child);
+                return true;
+            }else {
+                root = child;
+                return true;
+            }
+        }
+    }
+
+    public Student find(int id){
+        Node node = findNode(id);
+        if(node != null)
+            return node.getData();
+        return null;
+    }
+
+    private Node findNode(int id){
+        Node pointer = root;
+        while (pointer != null){
+            if(pointer.getData().getId() == id)
+                return pointer;
+            else if(pointer.getData().getId() > id)
+                pointer = pointer.getLeft();
+            else
+                pointer = pointer.getRight();
+        }
+        return null;
+    }
+
+    //TODO: Implement balanceTree
+    public boolean balanceTree(){
+        return false;
+    }
+
+    public void toFile(){
+
+    }
+
+    public static Tree fromFile(){
         return null;
     }
 }
