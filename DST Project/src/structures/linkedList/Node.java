c3117477d07d746063262d26727e942c3ad7acda
Azizcan Tam <tamazizcan407@gmail.com>
2024-05-01 20:09:28 +0300

    linkedlistnode

--- DST Project/src/structures/linkedList/Node.java
+++ DST Project/src/structures/linkedList/Node.java
@@ -5,14 +5,37 @@ public class Node {
 
     private double gpa;
     private Student data;
-    private  Node next;
+    private Node next;
 
-    public Node(Student data){
-        this.data=data;
-        this.gpa=data.getGpa;
-        this.next =null;
+    public Node(Student data) {
+        this.data = data;
+        this.gpa = data.getGpa;
+        this.next = null;
     }
 
+    public Student getData() {
+        return data;
+    }
 
+    public void setData(Student data) {
+        this.data = data;
+    }
 
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public src.structures.linkedList.Node getNext() {
+        return next;
+    }
+
+    public void setNext(src.structures.linkedList.Node next) {
+        this.next = next;
+    }
 }
+
+
