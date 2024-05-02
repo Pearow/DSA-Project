30affbe914548e5fec67759fe26246bc0f0881ff
Azizcan Tam <tamazizcan407@gmail.com>
2024-05-02 22:36:04 +0300

    linkedlistnode

--- DST Project/src/structures/linkedList/LinkedList.java
+++ DST Project/src/structures/linkedList/LinkedList.java
@@ -1,6 +1,8 @@
 package src.structures.linkedList;
 
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
    private Node root;
    private int lenght;
@@ -24,18 +26,18 @@ public class LinkedList {
 
    //İstersen
    //TODO: Get items from nodes according to their index
-    public <T> T get(int index, Class<T> Tclass){
-        if (index < 0 || head == null) {
+    public <T> T get(int index, Class<T> Tclass) throws NoSuchElementException {
+        if (index < 0 || root == null) {
             throw new NoSuchElementException();
         }
-        Node current = head;
+        Node current = root;
         for (int i = 0; i < index; i++) {
             if (current.next == null) {
                 throw new NoSuchElementException();
             }
             current = current.next;
         }
-        return Tclass.cast(current.data);
+        return Tclass.cast(current.getData());
     }
     
 
@@ -43,23 +45,25 @@ public class LinkedList {
 
     //TODO: Will be Fixed Later
     public <T> void remove(T data) {
-//      if (head == null) {
-//         return;
-//      }
-//      if (head.getData().equals(data)) {
-//         head = head.getNext();
-//         lenght--;
-//         return;
-//      }
-//      Node<T> current = head;
-//      while (current.getNext() != null) {
-//         if (current.getNext().getData().equals(data)) {
-//            current.getNext() = current.getNext().getNext();
-//            lenght--;
-//            return;
-//         }
-//         current = current.getNext();
-//      }
+     if (root == null) {
+         return;
+     }
+     if (root.getData().equals(data)) {
+        root = root.getNext();
+        lenght--;
+         return;
+     }
+     Node current = root;
+     Node prev = null;
+      while (current.getNext() != null) {
+        if (current.getData().equals(data)) {
+            prev.setNext(current.getNext());
+            lenght--;
+          return;
+        }
+        prev = current;
+        current = current.getNext();
+     }
    }
     public int getLenght() {
         return lenght;
