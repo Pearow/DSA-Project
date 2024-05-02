4bf2f2272ec023756d00356161a71efded125931
Azizcan Tam <tamazizcan407@gmail.com>
2024-05-02 22:57:04 +0300

    Changes by kerem

--- DST Project/src/structures/linkedList/LinkedList.java
+++ DST Project/src/structures/linkedList/LinkedList.java
@@ -31,7 +31,7 @@ public class LinkedList {
             throw new NoSuchElementException();
         }
         Node current = root;
-        for (int i = 0; i < index; i++) {
+        for (int i = 0; i <= index; i++) {
             if (current.getNext() == null) {
                 throw new NoSuchElementException();
             }
@@ -39,8 +39,24 @@ public class LinkedList {
         }
         return Tclass.cast(current.getData());
     }
-    
-    
+
+    public <T> boolean edit(int index, T data){
+        if (index < 0 || root == null) {
+            throw new NoSuchElementException();
+        }
+        Node current = root;
+        for (int i = 0; i <= index; i++) {
+            if (current.getNext() == null) {
+                throw new NoSuchElementException();
+            }
+            current = current.getNext();
+        }
+
+        current.setData(data);
+        return true;
+    }
+
+
     //TODO: Find the node with specified index and change its data to variable "data". Return true if completed succesfully else rtrn fls
 
     //TODO: Will be Fixed Later
