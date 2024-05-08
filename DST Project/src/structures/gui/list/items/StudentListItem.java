3d0f032c969f4f681c6a332fb83012c727c0d0f5
Pearow <salimmert2003@hotmail.com>
2024-05-08 21:39:31 +0300

    Student Item and List fix and changing folders

--- DST Project/src/structures/gui/list/items/StudentListItem.java
+++ DST Project/src/structures/gui/list/items/StudentListItem.java
@@ -1,4 +1,4 @@
-package src.structures.gui.studentList;
+package src.structures.gui.list.items;
 
 import src.Student;
 
@@ -19,19 +19,12 @@ public class StudentListItem extends JPanel{
         studentName = new JLabel(student.getName() + " " + student.getSurname());
         addToTeam = new JButton("+");
 
-
-        //Debug
-        if(cool){
-            setBackground(new Color(0, 0, 0, 15));
-            cool = false;
-        }else cool = true;
-
         //Set layout
         setLayout(new BorderLayout());
-//        setMinimumSize(new Dimension(100, 17));
         setPreferredSize(new Dimension(100, 17));
-//        setMaximumSize(new Dimension(1000, 17));
         setBorder(new EmptyBorder(0, 5, 0, 5));
+
+        setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0, 65), 1));
 
 
         add(studentName, BorderLayout.WEST);
@@ -41,11 +34,8 @@ public class StudentListItem extends JPanel{
     public static void main(String[] args) {
         JFrame frame = new JFrame("Student List Item");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200, 200);
+        frame.setSize(200, 41 + new StudentListItem(new Student()).getPreferredSize().height);
         frame.add(new StudentListItem(new Student(123456, 3.5,"John", "Doe", new Date(2000, 1, 1), "Computer Science", "Turkish", "123456")));
-        frame.add(new StudentListItem(new Student(1234562, 3.5,"John3", "Doe", new Date(2000, 1, 1), "Computer Science", "Turkish", "123456")));
-        frame.add(new StudentListItem(new Student(1234562, 3.5,"John3", "Doe", new Date(2000, 1, 1), "Computer Science", "Turkish", "123456")));
-        frame.add(new StudentListItem(new Student(1234562, 3.5,"John3", "Doe", new Date(2000, 1, 1), "Computer Science", "Turkish", "123456")));
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.setVisible(true);
     }
